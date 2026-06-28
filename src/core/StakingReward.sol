08738aaadafe15b157be4d739cfdac7a6e289b9a
Thien Le <thienle130702gmail.com>
2026-06-28 16:17:48 +0000

    fix bug erned function,add test: staking function

--- src/core/StakingReward.sol
+++ src/core/StakingReward.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {
+    ReentrancyGuard
+} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+contract StakingReward is ReentrancyGuard, Ownable {
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardToken;
 
-contract StakingReward is ReentrancyGuard, Ownable{
-    IERC20 immutable public stakingToken;
-    IERC20 immutable public rewardToken;
-    mapping (address => uint256) public stakedAmount;
-    mapping (address => uint256) public rewards;
-    mapping (address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public stakedAmount;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) public userRewardPerTokenPaid;
 
     uint256 constant REWARD_RATE = 100;
     uint256 public lastUpdateTime;
@@ -21,27 +25,31 @@ contract StakingReward is ReentrancyGuard, Ownable{
     error InvalidAmount();
     error InsufficientRewards();
 
-
-    constructor(address _stakingToken, address _rewardToken) Ownable(msg.sender){
+    constructor(
+        address _stakingToken,
+        address _rewardToken
+    ) Ownable(msg.sender) {
         stakingToken = IERC20(_stakingToken);
         rewardToken = IERC20(_rewardToken);
     }
-    
 
-    function erned(address account) public view returns (uint256){
-        return ((rewardPerTokenStored - userRewardPerTokenPaid[account]) * stakedAmount[account] / 1e18) + rewards[account];
+    function erned(address account) public view returns (uint256) {
+        return
+            (((rewardPerToken() - userRewardPerTokenPaid[account]) *
+                stakedAmount[account]) / 1e18) + rewards[account];
     }
 
-
-    function rewardPerToken() public view returns(uint256) {
-        if(totalStaked == 0){
+    function rewardPerToken() public view returns (uint256) {
+        if (totalStaked == 0) {
             return rewardPerTokenStored;
         }
-        return rewardPerTokenStored + (((block.timestamp - lastUpdateTime) * REWARD_RATE * 1e18) / totalStaked);
-    } 
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * REWARD_RATE * 1e18) /
+                totalStaked);
+    }
 
-
-    modifier updateReward(address account){
+    modifier updateReward(address account) {
         rewardPerTokenStored = rewardPerToken();
         lastUpdateTime = block.timestamp;
         rewards[account] = erned(account);
@@ -49,28 +57,33 @@ contract StakingReward is ReentrancyGuard, Ownable{
         _;
     }
 
-    function stake(uint256 amount) external nonReentrant updateReward(msg.sender){
+    function stake(
+        uint256 amount
+    ) external nonReentrant updateReward(msg.sender) {
         require(amount > 0, InvalidAmount());
         stakedAmount[msg.sender] += amount;
         totalStaked += amount;
-        SafeERC20.safeTransferFrom(stakingToken, msg.sender, address(this), amount);
+        SafeERC20.safeTransferFrom(
+            stakingToken,
+            msg.sender,
+            address(this),
+            amount
+        );
     }
 
-    function withdraw(uint256 amount) external nonReentrant updateReward(msg.sender){
+    function withdraw(
+        uint256 amount
+    ) external nonReentrant updateReward(msg.sender) {
         require(amount > 0, InvalidAmount());
         stakedAmount[msg.sender] -= amount;
         totalStaked -= amount;
         SafeERC20.safeTransfer(stakingToken, msg.sender, amount);
     }
 
-
-    function claimReward() public nonReentrant(){
+    function claimReward() public nonReentrant updateReward(msg.sender) {
         require(rewards[msg.sender] > 0, InsufficientRewards());
-        uint256 reward = rewards[msg.sender] ;
+        uint256 reward = rewards[msg.sender];
         rewards[msg.sender] = 0;
         SafeERC20.safeTransfer(rewardToken, msg.sender, reward);
     }
-
-
-
 }
---
+++ test/StakingReward.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {MockERC20} from "src/tokens/MockERC20.sol";
+import {RogStringX} from "src/tokens/RogStringX.sol";
+import {StakingReward} from "src/core/StakingReward.sol";
+
+contract testStakingReward is Test {
+    MockERC20 public stakeToken;
+    RogStringX public rewardToken;
+    StakingReward public stakingReward;
+    address alice ;
+    function setUp() public {
+        stakeToken = new MockERC20("LPToken", "LPT");
+        rewardToken = new RogStringX();
+        stakingReward = new StakingReward(
+            address(stakeToken),
+            address(rewardToken)
+        );
+
+        alice = makeAddr("alice");
+        stakeToken.mint(alice, 10000);
+        vm.prank(alice);
+        stakeToken.approve(address(stakingReward),1000);
+        rewardToken.mint(address(stakingReward), 1000000);
+        stakeToken.mint(address(this), 10000000);
+    }
+
+    function testStakingFunction() public {
+        stakeToken.approve(address(stakingReward), 200);
+        stakingReward.stake(200);
+        vm.prank(alice);
+        stakingReward.stake(1000);
+        vm.warp( block.timestamp + 1 days);
+        assertEq(stakingReward.erned(alice),0);
+        assertEq(stakingReward.totalStaked(),1200);
+        assertEq(stakingReward.stakedAmount(address(this)), 200);
+    }
+
+    function testWithdrawFunction() public {}
+
+    function testClaimFunction() public {}
+}
